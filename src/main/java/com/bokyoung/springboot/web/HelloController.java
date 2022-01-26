b4ccadc115e5668aa45297e2c350585160fa6a2a
bokyoung89 <sbk8689@gmail.com>
2022-01-26 21:19:27 +0900

    HelloController에 ResponseDto 적용

--- src/main/java/com/bokyoung/springboot/web/HelloController.java
+++ src/main/java/com/bokyoung/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.bokyoung.springboot.web;
 
+import com.bokyoung.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //컨트롤러를 json을 반환하는 컨트롤러로 만들어줌.
@@ -11,5 +13,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+/*      ### RequestParam ###
+        * 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+        * 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터를
+          메소드 파라미터 name(String name)에 저장한다.     */
+
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/bokyoung/springboot/web/HelloControllerTest.java
+++ src/test/java/com/bokyoung/springboot/web/HelloControllerTest.java
@@ -7,9 +7,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+### param ###
+* API 테스트할 때 사용될 요청 파라미터를 설정함.
+* 단, 값은 String만 허용됨.
+* 숫자/날짜의 데이터를 등록할 때는 문자열로 변경해야만 가능함.
+
+### jsonPath ###
+* JSON 응답값을 필드별로 검증할 수 있는 메소드
+* $를 기준으로 필드명을 명시함.
+* 여기서는 name과 amount를 검증하니 $.name, $.amount로 검증한다.
+*/
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -27,4 +41,16 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name =  "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
