f7047a5ca03dafd8b119ce332a2fb294df21a88b
bokyoung89 <sbk8689@gmail.com>
2022-02-14 23:47:23 +0900

    등록/수정/조회 api 클래스 생성

--- src/main/java/com/bokyoung/springboot/web/PostsApiController.java
+++ src/main/java/com/bokyoung/springboot/web/PostsApiController.java
@@ -18,8 +18,5 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
-        public Long save (@RequestBody PostsSaveRequestDto requestDto){
-            return postsService.save(requestDto);
-    }
+
 }
